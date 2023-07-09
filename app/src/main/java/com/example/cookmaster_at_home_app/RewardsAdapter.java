aa36525dfb470d7d3561096f4a54cb5351525768
Milo <lhilderal@myges.fr>
2023-07-09 02:37:28 +0200

    wip items

--- app/src/main/java/com/example/cookmaster_at_home_app/RewardsAdapter.java
+++ app/src/main/java/com/example/cookmaster_at_home_app/RewardsAdapter.java
@@ -1,29 +1,53 @@
 package com.example.cookmaster_at_home_app;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.ViewHolder> {
+public class RewardsAdapter extends RecyclerView.Adapter<RewardsAdapter.MyViewHolder> {
 
+    private List<Item> itemList;
     public RewardsAdapter(List<Item> itemList) {
+        this.itemList = itemList;
     }
+
     @NonNull
     @Override
-    public RewardsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RewardsAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.itemName.setText(item.getName());
+        //holder.itemImage.setImageResource(item.getImage()); use the library todo that
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout layout;
+        TextView itemName;
+        ImageView itemImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            layout = itemView.findViewById(R.id.item_layout);
+            itemName = itemView.findViewById(R.id.item_name);
+            itemImage = itemView.findViewById(R.id.item_image);
+        }
     }
 }
